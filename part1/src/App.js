3561ca02bb6355e4f6991c222ad1789226583b05
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-02-15 13:05:18 +0100

    first commit

---
+++ part1/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Mensaje from './Mensaje';
+
+const Description = () => {
+	return <p>Esta es la app del curso fullstack bootcamp</p>;
+};
+const App = () => {
+	return (
+		<div className="App">
+			<Mensaje color="red" message="Estamos trabajando" />
+			<Mensaje color="green" message="en un curso" />
+			<Mensaje color="yellow" message="de React" />
+			<Description />
+		</div>
+	);
+};
+
+export default App;
